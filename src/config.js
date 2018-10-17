08aeca709d5add593f1e51dcc13dccf52b629d2b
Jacky Fakkeldij <jackyfakkeldij@hotmail.com>
2018-10-17 23:03:12 +0200

    git ignore

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyBocCQy9Slnb6sv138g_yg-a9qEefwGJyw",
+  authDomain: "presentatieapp.firebaseapp.com",
+  databaseURL: "https://presentatieapp.firebaseio.com",
+  projectId: "presentatieapp",
+  storageBucket: "presentatieapp.appspot.com",
+  messagingSenderId: "788600908348"
 };
