46544a40ab437092286893c49db7d96e9819610a
Jacky Fakkeldij <jackyfakkeldij@hotmail.com>
2018-10-17 23:00:47 +0200

    config update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyBocCQy9Slnb6sv138g_yg-a9qEefwGJyw",
-  authDomain: "presentatieapp.firebaseapp.com",
-  databaseURL: "https://presentatieapp.firebaseio.com",
-  projectId: "presentatieapp",
-  storageBucket: "presentatieapp.appspot.com",
-  messagingSenderId: "788600908348"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
